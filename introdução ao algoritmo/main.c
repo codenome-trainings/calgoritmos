88e68287b5774152ae43a6424154c212e82862f5
Thiago Cunha <grupocodenome@gmail.com>
2015-06-11 17:20:44 -0300

    Printf e Scanf
    Faça um algoritmo que receba dois números e exiba o resultado da sua
    soma.

---
+++ introdução ao algoritmo/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int numUm, numDois;
+
+	printf("Digite o primeiro numero: \n");
+	scanf("%d", &numUm);
+	printf("Digite o segundo numero: \n");
+	scanf("%d", &numDois);
+
+	printf("A soma dos numeros digitados e: %d\n", numUm + numDois);
+}
